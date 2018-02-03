facff25f561cc84d1df66ead8d701d0c4c4a1b4f
Konstantin Läufer <klaeufer@gmail.com>
2018-02-03 17:08:46 -0600

    added initial version of working scanLeftAsIterator without lifting to Option
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/functional/modular/CumulativeLengthFunctionalModular.scala
+++ src/main/scala/functional/modular/CumulativeLengthFunctionalModular.scala
@@ -1,5 +1,7 @@
 package functional.modular
 
+import scala.collection.AbstractIterator
+
 /** Reads lines and prints cumulative length of all lines so far along with most recent line itself. */
 object CumulativeLengthFunctionalModular extends Main[(String, Int)] {
 
@@ -8,16 +10,36 @@ object CumulativeLengthFunctionalModular extends Main[(String, Int)] {
   // def scanLeft[B](z: B)(op: (B, A) ⇒ B): Iterator[B]
   // https://github.com/scala/scala/blob/v2.12.4/src/library/scala/collection/Iterator.scala#L595
 
-  def scanLeftAsIterate[T, U](it: Iterator[T])(z: U)(op: (U, T) => U): Iterator[U] =
+  def scanLeftUsingIterate[T, U](it: Iterator[T])(z: U)(op: (U, T) => U): Iterator[U] =
     Iterator.iterate(Option(z)) {
       case Some(r) =>
         if (it.hasNext) Option(op(r, it.next())) else None
     } takeWhile (_.isDefined) map (_.get)
 
+  def scanLeftAsIterator[T, U](it: Iterator[T])(z: U)(op: (U, T) => U): Iterator[U] = Iterator(z) ++ new AbstractIterator[U] {
+    private var hd: T = _
+    private var hdDefined = false
+    private var tail = it
+    private var elem = z
+
+    def hasNext = hdDefined || tail.hasNext && {
+      hd = tail.next()
+      hdDefined = true
+      hdDefined
+    }
+
+    def next() = if (hasNext) {
+      hdDefined = false
+      elem = op(elem, hd)
+      elem
+    } else
+      Iterator.empty.next()
+  }
+
   def accumulateCount(acc: (String, Int), line: String) = (line, acc._2 + line.length)
 
   def run(lines: Iterator[String]): Iterator[(String, Int)] =
-    scanLeftAsIterate(lines)("dummy", 0)(accumulateCount).drop(1)
+    scanLeftAsIterator(lines)("dummy", 0)(accumulateCount).drop(1)
 
   // lines.scanLeft(("dummy", 0))(accumulateCount).drop(1) // fail: delayed by one item
 }
