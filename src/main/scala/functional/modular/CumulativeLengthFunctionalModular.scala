4120eada82c337824e10746deabcf24fc80590a7
Konstantin Läufer <klaeufer@gmail.com>
2018-02-03 17:31:57 -0600

    completed scanLeftAsIterator by fusing ++, iterate, and takeWhile
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/functional/modular/CumulativeLengthFunctionalModular.scala
+++ src/main/scala/functional/modular/CumulativeLengthFunctionalModular.scala
@@ -6,40 +6,55 @@ import scala.collection.AbstractIterator
 object CumulativeLengthFunctionalModular extends Main[(String, Int)] {
 
   // def iterate[T](start: T)(f: (T) ⇒ T): Iterator[T]
-
   // def scanLeft[B](z: B)(op: (B, A) ⇒ B): Iterator[B]
   // https://github.com/scala/scala/blob/v2.12.4/src/library/scala/collection/Iterator.scala#L595
 
+  /**
+   * Simple implementation of `scanLeft` using `iterate` and `takeWhile`.
+   * For `takeWhile` to work, this requires lifting the resulting iterator to `Option`.
+   * Alternatively, one could use `null`, but that would be less stylish.
+   */
   def scanLeftUsingIterate[T, U](it: Iterator[T])(z: U)(op: (U, T) => U): Iterator[U] =
     Iterator.iterate(Option(z)) {
       case Some(r) =>
         if (it.hasNext) Option(op(r, it.next())) else None
     } takeWhile (_.isDefined) map (_.get)
 
-  def scanLeftAsIterator[T, U](it: Iterator[T])(z: U)(op: (U, T) => U): Iterator[U] = Iterator(z) ++ new AbstractIterator[U] {
+  /**
+   * From-scratch implementation of `scanLeft` as an `AbstractIterator` by essentially
+   * fusing `++` for the initial value, `iterate`, and `takeWhile`.
+   * This fusion results in a slightly complicated state space, but it works efficiently.
+   */
+  def scanLeftAsIterator[T, U](it: Iterator[T])(z: U)(op: (U, T) => U): Iterator[U] = new AbstractIterator[U] {
     private var hd: T = _
     private var hdDefined = false
     private var tail = it
+    private var initial = true
     private var elem = z
 
-    def hasNext = hdDefined || tail.hasNext && {
+    def hasNext = initial || hdDefined || tail.hasNext && {
       hd = tail.next()
       hdDefined = true
       hdDefined
     }
 
-    def next() = if (hasNext) {
-      hdDefined = false
-      elem = op(elem, hd)
-      elem
-    } else
-      Iterator.empty.next()
+    def next() =
+      if (initial) {
+        initial = false
+        z
+      } else if (hasNext) {
+        hdDefined = false
+        elem = op(elem, hd)
+        elem
+      } else
+        Iterator.empty.next()
   }
 
   def accumulateCount(acc: (String, Int), line: String) = (line, acc._2 + line.length)
 
   def run(lines: Iterator[String]): Iterator[(String, Int)] =
-    scanLeftAsIterator(lines)("dummy", 0)(accumulateCount).drop(1)
+    scanLeftUsingIterate(lines)("dummy", 0)(accumulateCount).drop(1)
 
-  // lines.scanLeft(("dummy", 0))(accumulateCount).drop(1) // fail: delayed by one item
+  // the following fails when running interactively: delayed by one line
+  // lines.scanLeft(("dummy", 0))(accumulateCount).drop(1)
 }
