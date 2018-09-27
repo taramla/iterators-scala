20091ba61b84d06226b8be710a482a91b32151ea
Konstantin Läufer <laufer@cs.luc.edu>
2018-09-27 15:37:28 -0500

    split last step into calculation of averages and printing

--- src/main/scala/functional/simple/CumAvgFunctional.scala
+++ src/main/scala/functional/simple/CumAvgFunctional.scala
@@ -3,12 +3,13 @@ package functional.simple
 object CumAvgFunctional extends App {
   val lines = scala.io.Source.stdin.getLines
   val values = lines.map(_.toDouble)
-  val results = values.scanLeft((0, 0.0)) {
-    case ((count, sum), value) =>
-      (count + 1, sum + value)
+  val countsWithSums = values.scanLeft((0, 0.0)) {
+    case ((count, sum), value) => (count + 1, sum + value)
   }
-  results.drop(1).foreach {
-    case (count, sum) =>
-      println(count + ": " + (sum / count))
+  val countsWithAvgs = countsWithSums.map {
+    case (count, sum) => (count, sum / count)
+  }
+  countsWithAvgs.drop(1).foreach {
+    case (count, avg) => println(count + ": " + avg)
   }
 }
