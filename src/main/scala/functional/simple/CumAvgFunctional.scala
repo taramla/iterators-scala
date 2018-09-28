18fcba2184a39ceb7dc9502a1395798887baa05d
Konstantin Läufer <laufer@cs.luc.edu>
2018-09-28 10:25:48 -0500

    added comments to functional cumulative average example

--- src/main/scala/functional/simple/CumAvgFunctional.scala
+++ src/main/scala/functional/simple/CumAvgFunctional.scala
@@ -2,13 +2,17 @@ package functional.simple
 
 object CumAvgFunctional extends App {
   val lines = scala.io.Source.stdin.getLines
+  // convert each input line to a number
   val values = lines.map(_.toDouble)
+  // for each number, produce a count and a cumulative sum
   val countsWithSums = values.scanLeft((0, 0.0)) {
     case ((count, sum), value) => (count + 1, sum + value)
   }
+  // convert the count and sum pairs to count and cumulative average pairs
   val countsWithAvgs = countsWithSums.map {
     case (count, sum) => (count, sum / count)
   }
+  // print the results except for the first one
   countsWithAvgs.drop(1).foreach {
     case (count, avg) => println(count + ": " + avg)
   }
